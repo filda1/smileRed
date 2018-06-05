b6e3249d3bdda427108f15314a787ea6a6872373
filda1 <38835920+filda1@users.noreply.github.com>
2018-06-05 21:21:28 +0100

    Update Offert.cs

--- Offert.cs
+++ Offert.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -14,27 +14,20 @@ namespace smileRed.Domain
         [Key]
         public int OffertId { get; set; }
 
-        //public int ProductId { get; set; }
+        public int ProductId { get; set; }
 
         [Display(Name = "Offer")]
         [Required(ErrorMessage = "The field {0} is requiered.")]
         [MaxLength(50, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
-        public string Name { get; set; }
+        public string Offer { get; set; }
 
         [Required(ErrorMessage = "The field {0} is required.")]
         [MaxLength(150, ErrorMessage = "The field {0} only can contain {1} characters lenght.")]
         [Index("Product_Description_Index", IsUnique = true)]
         public string Description { get; set; }
 
-        [DataType(DataType.Currency)]
-        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
-        [Required(ErrorMessage = "You must enter the field {0}")]
-        public decimal Price { get; set; }
-
         public string Image { get; set; }
-
-        public double Stock { get; set; }
-
+        
         [Display(Name = "Start Date")]
         [DataType(DataType.DateTime)]
         public DateTime StartDate { get; set; }
@@ -49,7 +42,7 @@ namespace smileRed.Domain
         [DataType(DataType.MultilineText)]
         public string Remarks { get; set; }
 
-       /* [JsonIgnore]
-        public virtual Product Product { get; set; }*/
+        [JsonIgnore]
+        public virtual Product Product { get; set; }
     }
 }
